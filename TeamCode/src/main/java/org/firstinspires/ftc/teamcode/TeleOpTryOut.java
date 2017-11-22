dee0dfd7667c9531defe35154d6a904095edf72c
DESKTOP-HCDS8UM\Vali <ai2000mk@gmail.com>
2017-11-22 16:58:14 +0200

    Really basic Op Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTryOut.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Andrei on 11/22/2017.
+ */
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+
+public class TeleOpTryOut extends LinearOpMode
+{
+    private DcMotor mot = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        mot  = hardwareMap.get(DcMotor.class, "mot");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            telemetry.addData( "Status" , "Motor On" );
+            telemetry.update();
+
+            mot.setPower(0.4);
+        }
+        telemetry.addData("Status" , "Terminated");
+        telemetry.update();
+        mot.setPower(0);
+    }
+}
